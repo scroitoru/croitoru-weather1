222bf2a918b16b6a06ff83087c5c6dd258b4a9b5
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-05-04 16:21:11 -0400

    added border

--- src/main/java/croitoru/openweathermap/WeatherApplication.java
+++ src/main/java/croitoru/openweathermap/WeatherApplication.java
@@ -11,7 +11,10 @@ public class WeatherApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/weather_application.fxml"));
-        root.setStyle("-fx-background-color:aliceblue;");
+        root.setStyle("-fx-background-color:aliceblue;"
+                        + "-fx-border-style: solid inside;"
+                        + "-fx-border-color: black;"
+                        + "-fx-border-width: 5;");
 
         Scene scene = new Scene(root, 600, 300);
 
--- src/main/java/croitoru/openweathermap/WeatherController.java
+++ src/main/java/croitoru/openweathermap/WeatherController.java
@@ -25,7 +25,7 @@ public class WeatherController {
     @FXML
     ImageView image1, image2, image3, image4, image5;
     @FXML
-    Label Descrip1, Descrip2, Descrip3, Descrip4, Descrip5;
+    Label descrip1, descrip2, descrip3, descrip4, descrip5;
 
     OpenWeatherMapService service;
 
@@ -55,11 +55,11 @@ public class WeatherController {
         ImageView currImage = new ImageView(forecast.list.get(0).weather.get(0).getIconUrl());
         todayImage.setImage(currImage.getImage());
         //next 5 days
-        Descrip1.setText(String.valueOf(forecast.getForcastFor(1).main.temp));
-        Descrip2.setText(String.valueOf(forecast.getForcastFor(2).main.temp));
-        Descrip3.setText(String.valueOf(forecast.getForcastFor(3).main.temp));
-        Descrip4.setText(String.valueOf(forecast.getForcastFor(4).main.temp));
-        Descrip5.setText(String.valueOf(forecast.getForcastFor(5).main.temp));
+        descrip1.setText(String.valueOf(forecast.getForcastFor(1).main.temp));
+        descrip2.setText(String.valueOf(forecast.getForcastFor(2).main.temp));
+        descrip3.setText(String.valueOf(forecast.getForcastFor(3).main.temp));
+        descrip4.setText(String.valueOf(forecast.getForcastFor(4).main.temp));
+        descrip5.setText(String.valueOf(forecast.getForcastFor(5).main.temp));
         image1.setImage(new Image(forecast.getForcastFor(1).weather.get(0).getIconUrl()));
         image2.setImage(new Image(forecast.getForcastFor(2).weather.get(0).getIconUrl()));
         image3.setImage(new Image(forecast.getForcastFor(3).weather.get(0).getIconUrl()));
