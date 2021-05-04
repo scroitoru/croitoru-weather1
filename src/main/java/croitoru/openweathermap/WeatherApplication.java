548f4b71d9ac27c616555f1174cbf84548cffc7a
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-05-04 14:29:44 -0400

    changed fonts and colors

--- src/main/java/croitoru/openweathermap/WeatherApplication.java
+++ src/main/java/croitoru/openweathermap/WeatherApplication.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class WeatherApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/weather_application.fxml"));
+        root.setStyle("-fx-background-color:aliceblue;");
 
         Scene scene = new Scene(root, 600, 300);
 
